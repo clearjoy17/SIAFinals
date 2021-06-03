6f19a4f9defd25f841e59194d8bfe3ecefb33390
fatimaya <lumayagfatima12@gmail.com>
2021-06-03 23:00:04 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,144 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="antialiased mt-1 flex flex-row justify-center text-center text-gray-700 font-sans font-light"
+  >
+    <div class="flex flex-col justify-center">
+      <div class="flex flex-row justify-center">
+        <button
+          class="m-5 p-2 rounded bg-red-500"
+          @click="changeSection('tinder')"
+        >
+          Tinder
+        </button>
+        <button
+          class="m-5 p-2 rounded bg-gray-300"
+          @click="changeSection('disliked')"
+        >
+          Disliked
+        </button>
+        <button
+          class="m-5 p-2 rounded bg-green-500"
+          @click="changeSection('liked')"
+        >
+          Liked
+        </button>
+      </div>
+
+      
+      <div v-if="section === 'liked'">
+        <div v-if="likedGifs.length === 0">No liked gifs 😭</div>
+        <div
+          v-else
+          v-for="likedGif in likedGifs.slice().reverse()"
+          v-bind:key="likedGif"
+        >
+          <img :src="likedGif.images.original.url" alt="" class="m-5 w-80" />
+        </div>
+      </div>
+
+      
+      <div v-else-if="section === 'disliked'">
+        <div v-if="dislikedGifs.length === 0">No disliked gifs 😇</div>
+        <div
+          v-else
+          v-for="dislikedGif in dislikedGifs.slice().reverse()"
+          v-bind:key="dislikedGif"
+        >
+          <img :src="dislikedGif.images.original.url" alt="" class="m-5 w-80" />
+        </div>
+      </div>
+
+     
+      <div v-else>
+        <div class="flex justify-center">
+          <button class="text-6xl" @click="swipeLeft">❌</button>
+
+          <div v-if="!Object.keys(gif).length">loading...</div>
+          <div v-else>
+            <img :src="gif.images.original.url" alt="" class="mx-5 w-80" />
+          </div>
+
+          <button class="text-6xl" @click="swipeRight">✅</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/styles/index.css';
+import { GiphyFetch } from '@giphy/js-fetch-api';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+  data() {
+    return {
+      section: 'tinder',
+      dislikedGifs: [],
+      likedGifs: [],
+      gif: {},
+    };
+  },
+
+  async created() {
+    await this.loadRandomGif();
+  },
+
+  methods: {
+    async loadRandomGif() {
+      const gf = new GiphyFetch('DPLg4iYU4YZwqCDl4Or5gI23HRKEmrv4');
+      const { data: gif } = await gf.random({ tag: 'movies' });
+      this.gif = gif;
+    },
+
+    changeSection(section) {
+      this.section = section;
+    },
+
+    gifIsNotSaved(gifArray, currentGifId) {
+      return !_.some(gifArray, (gif) => gif.id === currentGifId);
+    },
+
+    async swipeRight() {
+      if (this.gifIsNotSaved(this.likedGifs, this.gif.id)) {
+        this.likedGifs.push(this.gif);
+        if (this.likedGifs.length > 3) {
+          this.likedGifs.shift();
+        }
+      }
+      await this.loadRandomGif();
+    },
+
+    async swipeLeft() {
+      if (this.gifIsNotSaved(this.dislikedGifs, this.gif.id)) {
+        this.dislikedGifs.push(this.gif);
+        if (this.dislikedGifs.length > 3) {
+          this.dislikedGifs.shift();
+        }
+      }
+      await this.loadRandomGif();
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* font-sans */
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+
+  /* antialiased */
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+
+  /* text-center */
+  /* text-align: center; */
+
+  /* text-gray-800 */
+  /* color: #2c3e50; */
+
+  /* mt-20 */
+  /* margin-top: 60px; */
 }
 </style>
